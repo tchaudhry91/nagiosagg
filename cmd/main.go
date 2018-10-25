44155d5cba4296db1a84b4a601318182ec74d055
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2018-10-25 14:13:50 +0530

    Added main function to wire

---
+++ cmd/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"net/http"
+	"os"
+	"path/filepath"
+
+	"github.com/go-kit/kit/log"
+	"github.com/tchaudhry91/nagiosagg/svc"
+)
+
+func main() {
+	var (
+		httpAddr        = flag.String("http.addr", ":8080", "HTTP listen address")
+		nagiosStatusDir = flag.String("nagios_status_dir", "statuses", "Nagios Status Directory")
+		localDB         = flag.String("local_db", filepath.Join(os.TempDir(), "nagios.db"), "Filepath to store nagios status data in")
+	)
+	flag.Parse()
+
+	// Initialize Logger
+	var logger log.Logger
+	{
+		logger = log.NewLogfmtLogger(os.Stderr)
+		logger = log.With(logger, "ts", log.DefaultTimestampUTC)
+		logger = log.With(logger, "caller", log.DefaultCaller)
+	}
+
+	// Base Service
+	service, err := svc.NewNagiosParserSvc(*nagiosStatusDir, *localDB)
+	if err != nil {
+		logger.Log("err", err.Error())
+		panic("Failed to create service")
+	}
+
+	// Initialize router
+	r := svc.MakeHTTPHandler(service)
+
+	http.ListenAndServe(*httpAddr, r)
+
+}
